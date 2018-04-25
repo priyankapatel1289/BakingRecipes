496b55851fdb5a280506cd96747233cd74734085
Priyanka Patel <priyankapatel_89@yahoo.com>
2018-04-25 18:53:13 -0500

    implemented exoPlayer

--- app/src/main/java/com/example/priyanka/bakingrecipes/VideoInstructionsFragment.java
+++ app/src/main/java/com/example/priyanka/bakingrecipes/VideoInstructionsFragment.java
@@ -1,6 +1,8 @@
 package com.example.priyanka.bakingrecipes;
 
 
+import android.annotation.SuppressLint;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,6 +10,17 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.priyanka.bakingrecipes.models.StepsModel;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ConcatenatingMediaSource;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import java.util.ArrayList;
 
@@ -18,6 +31,13 @@ import java.util.ArrayList;
 public class VideoInstructionsFragment extends Fragment {
 
     private ArrayList<StepsModel> videoUrlList = new ArrayList<>();
+    private SimpleExoPlayerView playerView;
+    private SimpleExoPlayer player;
+
+    private boolean playWhenReady = true;
+    private int currentWindow;
+    private long playbackPosition;
+
 
 
     public VideoInstructionsFragment() {
@@ -30,6 +50,97 @@ public class VideoInstructionsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_video_instructions, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+        if (view != null) {
+            playerView = view.findViewById(R.id.video_view);
+
+            if (Util.SDK_INT > 23) {
+                initializePlayer();
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        hideSystemUi();
+        if ((Util.SDK_INT <= 23 || player == null)) {
+            initializePlayer();
+        }
+    }
+
+    @SuppressLint("InlineApi")
+    private void hideSystemUi() {
+        playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+        | View.SYSTEM_UI_FLAG_FULLSCREEN
+        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
+        }
+    }
+
+    private void releasePlayer() {
+        if (player != null) {
+            playbackPosition = player.getCurrentPosition();
+            currentWindow = player.getCurrentWindowIndex();
+            playWhenReady = player.getPlayWhenReady();
+            player.release();
+            player = null;
+        }
+    }
+
+    private void initializePlayer() {
+        if (player == null) {
+            player = ExoPlayerFactory.newSimpleInstance(
+                    new DefaultRenderersFactory(getContext()),
+                    new DefaultTrackSelector(),
+                    new DefaultLoadControl());
+            playerView.setPlayer(player);
+
+            player.setPlayWhenReady(playWhenReady);
+            player.seekTo(currentWindow, playbackPosition);
+        }
+
+        MediaSource[] mediaSourcesToLoad = new MediaSource[videoUrlList.size()];
+        for (int i = 0; i<videoUrlList.size(); i++) {
+            Uri uri = Uri.parse(videoUrlList.get(i).getVideoURL());
+            MediaSource mediaSource = buildMediaSource(uri);
+            mediaSourcesToLoad[i] = mediaSource;
+//            player.prepare(mediaSource, true, false);
+        }
+        MediaSource mediaSources = new ConcatenatingMediaSource(mediaSourcesToLoad);
+
+        if (player != null) {
+            player.prepare(mediaSources, true, false);
+        }
+
+    }
+
+    private MediaSource buildMediaSource(Uri uri) {
+        return new ExtractorMediaSource.Factory(
+                new DefaultHttpDataSourceFactory("exo-player"))
+                .createMediaSource(uri);
     }
 
     public void setVideoUrlList(ArrayList<StepsModel> list) {
